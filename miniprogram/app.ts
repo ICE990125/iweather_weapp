de589a6907b6db4f0a527d8c88e02b53580a2b4e
ARCTURUS <1638330246@qq.com>
2022-05-16 21:24:07 +0800

    feat: 全局 data 中加入天气请求类

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,6 +1,13 @@
 import './utils/time'
+import { appKey } from './appKey' // 导入密钥
+import QWeatherStrategies from './utils/weather/strategies/qweather'
+import Weather from './utils/weather/strategies/weather'
 
 App<IAppOption>({
   globalData: {},
-  onLaunch() {},
+  onLaunch() {
+    const qw: QWeatherStrategies = new QWeatherStrategies(appKey['qWeather'])
+
+    this.globalData.weather = new Weather(qw)
+  },
 })
