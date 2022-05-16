d68e67bc9bcae2b774c3ae2c5e0ca2e1ec0347ba
ARCTURUS <1638330246@qq.com>
2022-05-16 21:25:55 +0800

    refactor: [接口] 修改 IAppOption 中 globalData 的定义

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,15 +2,27 @@
 /// <reference path="./weather/index.d.ts" />
 /// <reference path="./location/index.d.ts" />
 
-type methods = "GET" | "OPTIONS" | "HEAD" | "POST" | "PUT" | "DELETE" | "TRACE" | "CONNECT" | undefined
+type methods =
+  | 'GET'
+  | 'OPTIONS'
+  | 'HEAD'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+  | 'TRACE'
+  | 'CONNECT'
+  | undefined
+
 type wColor = Record<string, string>
 
 interface Date {
-    format(fmt: string): string
-    week(): string
-    deltaDay(n: number): Date
+  format(fmt: string): string
+  week(): string
+  deltaDay(n: number): Date
 }
 
 interface IAppOption {
-    globalData: {};
+  globalData: {
+    [key: string]: any
+  }
 }
