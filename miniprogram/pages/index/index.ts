d01e99d33a973cfbe15b0dc99cda1810e612ad8d
ARCTURUS <1638330246@qq.com>
2022-05-29 20:13:38 +0800

    refactor: 删掉测试用例

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,16 +1,4 @@
-import Location from '../../utils/location'
-
-const app = getApp()
-
 Page({
   data: {},
-  onLoad() {
-    const loc = new Location(20, 110)
-    app.globalData.weather.getAllweather(loc).then((res: any) => {
-      console.log(res)
-      this.setData({
-        weather: res,
-      })
-    })
-  },
-})
+  onLoad() {},
+});
