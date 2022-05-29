dfcb51ae3f2b47d1645f39cf787c3bb893cd50df
ARCTURUS <1638330246@qq.com>
2022-05-29 20:14:59 +0800

    feat: 改成英文说明

--- miniprogram/utils/weather/color.ts
+++ miniprogram/utils/weather/color.ts
@@ -1,46 +1,31 @@
 export class WaringColor {
-  // 未知预警
-  static unknown: wColor = {
+  // unknown
+  static unknown = {
     backgroundColor: '#EEEEEE',
     color: '#909399',
-  }
+  };
 
-  //  蓝色预警
-  static minor: wColor = {
+  //  blue
+  static minor = {
     backgroundColor: '#C6E5FC',
     color: '#2598F3',
-  }
+  };
 
-  //  黄色预警
-  static moderate: wColor = {
+  //  yellow
+  static moderate = {
     backgroundColor: '#FEEDD7',
     color: '#FB8900',
-  }
+  };
 
-  //  橙色预警
-  static severe: wColor = {
+  //  orange
+  static severe = {
     backgroundColor: '#FFEBE5',
     color: '#FF7347',
-  }
+  };
 
-  //  红色预警
-  static extreme: wColor = {
+  //  red
+  static extreme = {
     backgroundColor: '#FFD3D3',
     color: '#FF5353',
-  }
-
-  static getWaringColor(level: string): wColor {
-    switch (level) {
-      case '蓝色':
-        return WaringColor.minor
-      case '黄色':
-        return WaringColor.moderate
-      case '橙色':
-        return WaringColor.severe
-      case '红色':
-        return WaringColor.extreme
-      default:
-        return WaringColor.unknown
-    }
-  }
+  };
 }
