d13ca525e40c5d227b634eb7236b436258713577
ARCTURUS <1638330246@qq.com>
2022-05-29 20:16:24 +0800

    feat: 新增声明

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,28 +1,9 @@
 /// <reference path="./wx/index.d.ts" />
-/// <reference path="./weather/index.d.ts" />
-/// <reference path="./location/index.d.ts" />
 
-type methods =
-  | 'GET'
-  | 'OPTIONS'
-  | 'HEAD'
-  | 'POST'
-  | 'PUT'
-  | 'DELETE'
-  | 'TRACE'
-  | 'CONNECT'
-  | undefined
-
-type wColor = Record<string, string>
-
-interface Date {
-  format(fmt: string): string
-  week(): string
-  deltaDay(n: number): Date
-}
+interface ILocation {}
 
 interface IAppOption {
   globalData: {
-    [key: string]: any
-  }
+    [key: string]: any;
+  };
 }
