b9794b4773ee46b7edb74346c4a97dc5d4c862bc
ARCTURUS <1638330246@qq.com>
2022-05-29 20:12:47 +0800

    test: 添加测试用例

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,13 +1,13 @@
-import './utils/time'
-import { appKey } from './appKey' // 导入密钥
-import QWeatherStrategies from './utils/weather/strategies/qweather'
-import Weather from './utils/weather/strategies/weather'
+import { qWeatherKey } from './appKey'; // 导入密钥
+import QWeatherStrategies from './utils/weather/strategies/qweather';
+import Weather from './utils/weather/strategies/weather';
+import './utils/weather/test/index';
 
 App<IAppOption>({
   globalData: {},
   onLaunch() {
-    const qw: QWeatherStrategies = new QWeatherStrategies(appKey['qWeather'])
+    const qw: QWeatherStrategies = new QWeatherStrategies(qWeatherKey);
 
-    this.globalData.weather = new Weather(qw)
+    this.globalData.weather = new Weather(qw);
   },
-})
+});
